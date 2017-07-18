c7c2e2640046eec1a47c59785c0c0faa9abcab9f
andela-aonifade <anuoluwapo.onifade@andela.com>
2017-07-18 11:12:08 +0100

    Adds sign in and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+
+  get 'dashboard/index'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  resources :users
+
+  get 'dashboard' => 'dashboard#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'dashboard#index'
 end
