8dfc6a1eb47080fcbbb4980f155ed243b49fe827
Haruna Popoola <haruna.popoola@andela.com>
2017-12-07 16:38:11 +0100

    fix(adminPage): design all landing pages - fix footer for login and signup page [Fixes #150952242]

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,13 @@
 class AdminController < ApplicationController
   def index
   end
+
+  def month_details
+  end
+
+  def total_contributions
+  end
+
+  def total_loans
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  scope '/admin' do
+    get "/month-details" => "admin#month_details", as: "admin_month_details"
+    get "/application-details" => "admin#application_details", as: "admin_application_details"
+    get "/total-contributions" => "admin#total_contributions", as: "admin_total_contributions" 
+    get "/total-loans" => "admin#total_loans", as: "admin_total_loans"
+    get "/all-users" => "admin#all_users", as: "admin_all_users"
+  end
   resources :admin
   resources :contributions
   resources :loans
@@ -11,7 +18,12 @@ Rails.application.routes.draw do
   resources :profiles
   resources :loan_types
   resources :users
-    get 'dashboard' => 'dashboard#index'
+
+  get 'dashboard' => 'dashboard#index'
+  get 'recent-loans' => 'loans#recent_loans'
+  get 'notifications' => 'notifications#notifications'
+  get 'messages' => 'notifications#messages'
+  get 'support' => 'supports#index'
 
   controller :sessions do
     get "login" => :new
