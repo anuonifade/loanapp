422bf177b116bc99e7806046ddac26c8ef50d573
andela-aonifade <anuoluwapo.onifade@andela.com>
2019-02-02 14:29:01 +0100

    adds total contribution to user profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -57,6 +57,7 @@ class ProfilesController < ApplicationController
     user = User.find(@user_id)
     user_id = params[:id] if params[:id] && user.role_id < 3
     @profile = Profile.find_by(user_id: user_id) || nil
+    @total_contribution = MonthlyContribution.where(profile_id: @profile.id).sum(:amount) || 0.00
 
     @thrift_account = @staff_id
     @thrift_email = @user_email
